74123c32830974328c15ac3851b1dd14221f3935
Deepak-glitch-123 <dk.deepak2093@gmail.com>
2024-10-21 23:30:43 +0530

    Update python.py

---
+++ python.py
@@ -0,0 +1,10 @@
+def add_numbers(a, b):
+    return a + b
+
+# Input numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Call the function and print the result
+result = add_numbers(num1, num2)
+print("The sum is:", result)
